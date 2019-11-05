fc45e23e2ae0078080a617ef44b4b422a81becf8
dima <dima@fedoto.ws>
2019-11-05 18:40:02 +0200

    input validation

--- src/main/java/bullsandcows/PrimaryController.java
+++ src/main/java/bullsandcows/PrimaryController.java
@@ -1,9 +1,13 @@
 package bullsandcows;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.TableView;
 
@@ -14,13 +18,33 @@ public class PrimaryController {
     public Spinner<Integer> num3;
     public Spinner<Integer> num4;
     public TableView<Turn> turns;
+    public Button goButton;
+
+    public void initialize() {
+        goButton.disableProperty().bind(
+                Bindings.createBooleanBinding(this::invalidNumbers,
+                        num1.valueProperty(),
+                        num2.valueProperty(),
+                        num3.valueProperty(),
+                        num4.valueProperty())
+        );
+    }
+
+    private Boolean invalidNumbers() {
+        var numSet = new HashSet<Integer>();
+        numSet.add(num1.getValue());
+        numSet.add(num2.getValue());
+        numSet.add(num3.getValue());
+        numSet.add(num4.getValue());
+        return numSet.size() < 4;
+    }
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
 
-    public void doTurn(ActionEvent actionEvent) {
+    public void doTurn(ActionEvent actionEvent) throws IOException {
         turnCounter++;
         int n1 = num1.getValue();
         int n2 = num2.getValue();
--- src/main/java/bullsandcows/SecondaryController.java
+++ src/main/java/bullsandcows/SecondaryController.java
@@ -1,9 +1,31 @@
 package bullsandcows;
 
-import java.io.IOException;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+import static javafx.beans.binding.Bindings.*;
+import static javafx.beans.binding.Bindings.isNotEmpty;
+import static javafx.beans.binding.Bindings.when;
 
 public class SecondaryController {
+
+    public TextField myTextField;
+    public Label myTextLablel;
+
+    public void initialize() {
+        myTextLablel.textProperty()
+                .bind(when(isNotEmpty(myTextField.textProperty()))
+                        .then(concat(
+                                "Hello, ",
+                                myTextField.textProperty(),
+                                "!"))
+                        .otherwise("Enter your name!")
+                );
+    }
 
     @FXML
     private void switchToPrimary() throws IOException {
